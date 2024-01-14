3cd783c5bfcd88b47ca6f09ebddae55492666a36
Pedro Carvalho <eng.pedro.amb@gmail.com>
2024-01-14 17:12:44 +0100

    add email validation for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'must be a valid email address' }
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   has_many :requests
