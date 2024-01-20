4f06895a9b23e61a9d02022855cc56ebb7828315
Pedro Carvalho <eng.pedro.amb@gmail.com>
2024-01-20 14:29:14 +0100

    format responses controller js

--- app/javascript/controllers/requests_controller.js
+++ app/javascript/controllers/requests_controller.js
@@ -3,15 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "url", "response" ]
 
-
   connect() {
-    console.log("Connected")
-  }
+    console.log("Connected");
+}
 
   makeRequest() {
-    event.preventDefault()
+    event.preventDefault();
 
-    const url = this.urlTarget.value
+    const url = this.urlTarget.value;
 
     if (url === "") {
       alert("URL cannot be blank");
@@ -27,19 +26,17 @@ export default class extends Controller {
       },
       body: JSON.stringify({ url: url })
     })
-    .then(response =>
-      response.json()
-    )
+    .then(response => response.json())
     .then(data => {
       const formattedJson = JSON.stringify(JSON.parse(data.body), null, 2);
-      this.responseTarget.querySelector('code').textContent = `${formattedJson}`
+      this.responseTarget.querySelector('code').textContent = `${formattedJson}`;
       Prism.highlightElement(this.responseTarget.querySelector('code'));
     })
     .catch(error => {
       console.log(error);
       this.responseTarget.querySelector('code').textContent = `
       Something went wrong with the request, check endpoint!
-      `
-    })
+      `;
+    });
   }
 }
