8a241476d9f0016f815c69943be1cd9d2ed7aa51
Pedro Carvalho <eng.pedro.amb@gmail.com>
2024-01-20 14:29:47 +0100

    add dependent destroy to request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,7 @@ class Request < ApplicationRecord
   METHODS = %w[GET]
   belongs_to :user
 
-  has_one :response
+  has_one :response, dependent: :destroy
 
   validates :url, presence: true
   # validates :method, inclusion: { in: METHODS }
